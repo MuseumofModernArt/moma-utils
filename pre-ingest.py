06a25b0986ff06e7a7bf970582dc1d9bf842f3a2
Ben Fino-Radin <bfinoradin@gmail.com>
2013-09-24 09:53:07 -0400

    now uses DRMC naming convention via TMS API

--- pre-ingest.py
+++ pre-ingest.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-import bagit, datetime, time, argparse, os, sys, subprocess, filecmp, hashlib
+import bagit, datetime, time, argparse, os, sys, subprocess, filecmp, hashlib, threading, json, urllib2
 
 epoch = time.time()
 timestamp = datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d_%H:%M:%S')
 
 parser = argparse.ArgumentParser(description="Python tool for ingest from shuttle hard drives at MoMA")
 parser.add_argument('-i', '--input', type=str, required=True, help='The full path to the materials on the shuttle drive you wish to transfer.')
+parser.add_argument('-id', '--accessionid', type=str, required=True, help='objectid of the work.')
 parser.add_argument('-o', '--output', type=str, default='~/Desktop/', help='Location you wish to store the bagged transfer. Defaults to desktop if not specified')
 parser.add_argument('-n', '--name', type=str, help='Name of the person operating the script. This ends up the Bag metadata')
 parser.add_argument('-t', '--title', type=str, default='untitled_shuttledrive_transfer', help='Name of the transfer. This is optional.')
 args = parser.parse_args()
+
 
 
 def comp(list1, list2):
@@ -19,76 +21,87 @@ def comp(list1, list2):
 			return True
 	return False
 
-
 def md5_for_file(f, block_size=8192):
 	md5 = hashlib.md5()
-	for i in range (100):
-		while True:
-			data = f.read(block_size)
-			if not data:
-				break
-			md5.update(data)
-		sys.stdout.write("\r%d%%" %i)
-		sys.stdout.flush()
+	while True:
+		data = f.read(block_size)
+		if not data:
+			break
+		md5.update(data)
 	return md5.hexdigest()
 
 def execute(command):
 	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# Poll process for new output until finished
-	while True:
-		nextline = process.stdout.readline()
-		if nextline == '' and process.poll() != None:
-			break
-		sys.stdout.write(nextline)
-		sys.stdout.flush()
-	output = process.communicate()[0]
-	exitCode = process.returncode
-	if (exitCode == 0):
-		return output
-	else:
-		print "rysnc failed"
 
-dirname = timestamp+'__'+args.title
+
+class RepeatingTimer(threading._Timer):
+	def run(self):
+			while True:
+				self.finished.wait(self.interval)
+				if self.finished.is_set():
+					return
+				else:
+					self.function(*self.args, **self.kwargs)
+
+def status():
+	print (""),
+
+def bag_that():
+	bagit.make_bag(fullpath, {'Contact-Name': args.name})
+
+
+def hash_that():
+	## read md5 into list
+	baghashes = []
+	orighashes = []
+	for line in open(fullpath+"/manifest-md5.txt"):
+		column = line.split("  ")
+		baghashes.append(column[0])
+	
+	for path, subdirs, files in os.walk(args.input):
+		for name in files:
+			origpath = os.path.join(path, name)
+			f = open(origpath)
+			thishash = md5_for_file(f)
+			orighashes.append(thishash)
+	if comp (baghashes, orighashes) == False:
+		print "Something went wrong. There is a mismatch between the bag hashes and hashes of the files on the original storage media."
+	else:
+		print "hashes match!"
+
+objectid = args.accessionid
+req = json.load(urllib2.urlopen("http://vmsqlsvcs.museum.moma.org/TMSAPI/TmsObjectSvc/TmsObjects.svc/GetTombstoneDataRest/Object/"+objectid))
+artistname = req["GetTombstoneDataRestResult"]["DisplayName"]
+worktitle = req["GetTombstoneDataRestResult"]["Title"]
+objectnum = req["GetTombstoneDataRestResult"]["ObjectNumber"]
+objectid = req["GetTombstoneDataRestResult"]["ObjectID"]
+verbatim = artistname+"---"+worktitle+"---"+objectnum+"---"+objectid
+sanitized = verbatim.replace (" ", "_")
+
+# dirname = timestamp+'__'+args.title
+dirname = sanitized
 fullpath = os.path.expanduser(os.path.normpath(args.output) + os.sep)+dirname
+
 
 if not os.path.exists(fullpath):
 	os.makedirs(fullpath)
 	print "Made "+fullpath+" directory."
 
-print "copying file(s)"
+print "\n==========================\ncopying file(s) with rsync \n=========================="
+timer = RepeatingTimer(1.0, status)
+timer.daemon = True
+timer.start()
+proc = subprocess.Popen(["rsync", "-avP", os.path.expanduser(os.path.normpath(args.input)), fullpath])
+proc.wait()
+timer.cancel()
 
-execute(["rsync", "-avP", os.path.expanduser(os.path.normpath(args.input)), fullpath])
 
-# subprocess.call(["rsync", "-avP", "--partial", args.input, fullpath])
 
-print "File(s) copied... bagging local copy..."
-bagit.make_bag(fullpath, {'Contact-Name': args.name})
+print 'Bagging files....  ',
+bag_that()
+print '\b\b files bagged!'
 
-print "Files bagged... making list of bag hashes."
-## read md5 into dictionary
-baghashes = []
-orighashes = []
-for line in open(fullpath+"/manifest-md5.txt"):
-	column = line.split("  ")
-	baghashes.append(column[0])
+print 'Verifying hashes....  ',
+hash_that()
 
-print "Calculating MD5 hashes of files on original source media."
-## calculate md5 for files on original media
-for path, subdirs, files in os.walk(args.input):
-	for name in files:
-		origpath = os.path.join(path, name)
-		f = open(origpath)
-		thishash = md5_for_file(f)
-		orighashes.append(thishash)
-		#
-		# thishash = hashlib.md5()
-		# f = open(origpath)
-		# while not endOfFile:
-		# 	data = f.read(8192)
-		# 	thishash.update(data)
 
-print "Comparing hashes"
-if comp (baghashes, orighashes) == True:
-	print "All is well. The bagged files match the files on the original storage media."
-else:
-	print "Something went wrong. Checksum mismatch."
