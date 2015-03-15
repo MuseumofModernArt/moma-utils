93e52ab96e5079222d0635838511eca5f3e396f8
Ben Fino-Radin <bfinoradin@gmail.com>
2015-03-15 19:01:40 -0400

    time difference, dict, AIP size

--- auto-bench.py
+++ auto-bench.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import argparse, re, time,ast
+import argparse, re, time,ast,json,urllib2
+
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description="Python tool for generating performance statistics from Archivematica's Automation-tools log file")
 parser.add_argument('-i', '--input', type=file, help='log file to read')
@@ -11,54 +13,76 @@ if not (args.input):
 
 log = args.input
 
-# x = 0
-# l = 0
-
-# regex1 = re.compile("New transfer candidates: set\(\['(.+)'\]\)")
-# regex2 = re.compile(r'(\S+)\s+(\S{10} \S{8})\s*(\S.*)$')
-
-
-# for line in log:
-# 	start_transfer = regex1.search(line)
-# 	timestamp = regex2.search(line)
-# 	if start_transfer:
-# 		l = l+1
-# 		n = start_transfer.group(1)
-# 		ntrim = n.split("/")[1]
-# 		if timestamp:
-# 			print timestamp.group(2)+" "+ntrim+"   line:  "+str(x)
-# 	x = x+1
-# print "total lines: "+str(x)
-# print "lines where transfers are started: "+str(l)
-
 x = 0
+y = 0
+start_time = 0
+stop_time = 0
+trans_dict = dict()
+
+def start():
+	candidates = start_match.group(0)
+	startidno = candidates.split("/")[1][:-3]
+	start_time =  timestamp
+	# print startidno+' started at '+start_time
+	return (start_time, startidno)
+
+def stop():
+	info = stop_match.group(2)
+	stopidno = info[10:]
+	stop_time = timestamp
+	uuid = stop_match.group(4)
+	# print stopidno+' finished at '+stop_time+'\n'
+	return (stop_time, stopidno, uuid)
 
 for line in log:
 	match = re.match(r'(\S+)\s+(\S{10} \S{8})\s*(\S.*)$', line)
 	if match:
 		level = match.group(1)
 		timestr = match.group(2)
-		timestamp = time.mktime(time.strptime(timestr, '%Y-%m-%d %H:%M:%S'))
+		# timestamp = time.mktime(time.strptime(timestr, '%Y-%m-%d %H:%M:%S'))
+		timestamp = datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S')
 		message = match.group(3)
 		
-		# # transfer candidates
-		# match = re.match(r'.*New transfer candidates: set\((.*/.*)\)', message)
-		# if match:
-		# 	candidates = match.group(0)
-		# 	candidates = candidates.split("/")[1][:-3]
-		# 	if len(candidates) < 40:
-		# 		print 'New transfer candidate at '+str(timestamp)+': ', candidates
-		# 		x = x+1
-		# 	continue
-
-		# status info
-		match = re.match(r'.*Status info: (.*)$', message)
-		if match:
-			info = ast.literal_eval(match.group(1))
-			print 'Status info:', info
+		# find when new candidates are set
+		start_match = re.match(r'.*New transfer candidates: set\((.*/.*)\)', message)
+		if start_match:
+			start_time = start()[0]
+			start_idno = start()[1]
+			x = x+1
+			# add idno to dictionary as key, add starttime as value 1
+			trans_dict[start_idno] = [start_time,]
 			continue
 
-	# 	print 'Unrecognized message.'
-	# else:
-	# 	print 'Unrecognized line.'
-print x
+		# find when something is marked as complete
+		stop_match = re.match(r"(.*u'status': u'COMPLETE'.*)\s*('name': u'.*---.*---.*)\s*(.*', u'path'.*)\s*(.*'.*-.*-.*-.*-.*)", message)
+		if stop_match:
+			stop_time = stop()[0]
+			stop_idno = stop()[1]
+			uuid = stop()[2][:-2][1:]
+			# where idno matches in dict, add stoptime and UUID as values 2 and 3
+			if trans_dict.has_key(stop_idno):
+				trans_dict[stop_idno].append(stop_time)
+				trans_dict[stop_idno].append(uuid)
+				y = y+1
+			continue
+
+for key in trans_dict:
+	if len(trans_dict[key]) > 1:
+		uuid = str(trans_dict[key][2])
+		req = json.load(urllib2.urlopen("http://drmc.museum.moma.org/api/aips/"+uuid))
+		print req
+		AIPsize = req["size"]
+		print "AIP UUID: "+uuid
+		print "AIP Name: "+key
+		print "Is this size: "+AIPsize
+		# duration = float(trans_dict[key][1])-float(trans_dict[key][0])
+		duration = trans_dict[key][1]-trans_dict[key][0]
+		print "Took this long: "+str(duration)+"\n"
+
+			
+# print str(x)+" starts"
+# print str(y)+" stops"
+# print trans_dict
+		
+		
+		# print finish_time - start_time
