c72667c9918b2aa75dd15f5999f8908a6d6db6cd
Ben Fino-Radin <bfinoradin@gmail.com>
2015-03-30 14:17:01 -0400

    working draft

--- safe-delete.py
+++ safe-delete.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from requests.exceptions import HTTPError
+import sqlite3 as lite
+import sys, argparse, requests, getpass, csv
+
 
 # The purpose of this script is to provide a report of what transfers
 # have been sucsesfully processed by Archivematica's automation-tools,
@@ -15,17 +20,50 @@
 # point in the workflow (for instance, normalization, or AIP storage),
 # automation-tools is ignorant of this fact.
 
-# What follows is some pseudo-code...
+parser = argparse.ArgumentParser(description="Python tool for auditing Automation-Tool's database and making sure AIP is stored and tracked in Binder")
+parser.add_argument('-i', '--input', type=str, help='sqlite database to read')
+parser.add_argument('-u', '--username', type=str, help='Binder username')
+args = parser.parse_args()
 
-open csv for writing
-open anothercsv for writing and reading
+if not (args.input):
+	parser.error('you did not specify a database')
+if not (args.username):
+	parser.error('you did not supply a username')
 
-for row in sqliteDB:
-	read UUID of transfer
-	ping Binder API with UUID of transfer (AIP will have same UUID)
-	if Binder responds saying that it has this AIP:
-		write row in csv with UUID, and construct the theoretical path to the transfer source to be deleted
-	else:
-		write row in anothercsv with UUID
-		delete this row from sqliteDB
+password = getpass.getpass("Enter your password: ")
 
+db = args.input
+conn = lite.connect(db)
+c = conn.cursor()
+deletion_count = 0
+reingest_count = 0
+csvfile = open('delete_me.csv', 'wb')
+writer = csv.writer(csvfile)
+writer.writerow(['AIP uuid', 'original transfer path'])
+
+# row headers are
+# id, uuid, path, unit_type, status, microservice, current
+for row in c.execute('SELECT * FROM unit where unit_type = "ingest"'):
+	uuid = row[1]
+	path = row[2]
+	username = args.username
+	auth = (username, password)
+	url = "http://drmc.museum.moma.org/api/aips/"+uuid
+	try:
+		r = requests.get(url, auth=auth)
+		r.raise_for_status()
+		deletion_count += 1
+		print (path+" can be deleted \n")
+		# write row in CSV with AIP UUID and path
+		writer.writerow([uuid, path])
+	except HTTPError:
+		print ('Could not find in Binder', r.url,'\n')
+		reingest_count = reingest_count+1
+		# write row in anothercsv with UUID
+		# delete this row from sqliteDB
+print (str(deletion_count)+" transfer sources that can be deleted")
+print (str(reingest_count)+" transfers that need to be re-ingested")
+
+
+# delete from unit where uuid = "4d8f31ad-1c77-4772-be14-40618d25e481";
+# select * from unit where uuid = "4d8f31ad-1c77-4772-be14-40618d25e481";
