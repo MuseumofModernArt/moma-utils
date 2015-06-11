04c7ef9066ebf09797a8f5adb373966c37b0cd80
Ben Fino-Radin <bfinoradin@gmail.com>
2015-06-11 12:14:57 -0400

    edit

--- tmux.sh
+++ tmux.sh
@@ -7,16 +7,15 @@ split-window -d -t 0 -v
 split-window -d -t 2 -v
 
 send-keys -t 0 'workon my_virtualenv' enter C-l
-send-keys -t 0 'python manage.py runserver' enter
+send-keys -t 0 'htop' enter
 
 send-keys -t 1 'htop' enter C-l
 
 send-keys -t 2 'workon my_virtualenv' enter C-l
-send-keys -t 2 'python manage.py celery worker --loglevel=info' enter
+send-keys -t 2 'htop' enter
 
 send-keys -t 3 'workon my_virtualenv' enter C-l
-send-keys -t 3 'cd MyProject/webui/' enter
-send-keys -t 3 'brunch watch' enter
+send-keys -t 3 'htop' enter
 
 ## Just a convenience shell
 send-keys -t 4 'workon my_virtualenv' enter C-l
