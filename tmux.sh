e7eab91785dc243ca50b0ea0803ad9837e9ca7be
Ben Fino-Radin <bfinoradin@gmail.com>
2015-06-11 12:17:01 -0400

    testing

--- tmux.sh
+++ tmux.sh
@@ -6,19 +6,16 @@ split-window -d -t 0 -h
 split-window -d -t 0 -v
 split-window -d -t 2 -v
 
-send-keys -t 0 'workon my_virtualenv' enter C-l
 send-keys -t 0 'htop' enter
 
 send-keys -t 1 'htop' enter C-l
 
-send-keys -t 2 'workon my_virtualenv' enter C-l
 send-keys -t 2 'htop' enter
 
-send-keys -t 3 'workon my_virtualenv' enter C-l
 send-keys -t 3 'htop' enter
 
 ## Just a convenience shell
-send-keys -t 4 'workon my_virtualenv' enter C-l
+send-keys -t 4 'htop' enter C-l
 select-pane -t 4
 
 attach
