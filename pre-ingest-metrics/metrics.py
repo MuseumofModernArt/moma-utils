654bc2e0ea5be588f8cbc1c1b16a295f8e3acb14
Ben Fino-Radin <bfinoradin@gmail.com>
2015-06-12 12:52:02 -0400

    production path

--- pre-ingest-metrics/metrics.py
+++ pre-ingest-metrics/metrics.py
@@ -8,7 +8,7 @@ def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
 
-base_path = '/Volumes/drmc/Collections_materials/'
+base_path = '/home/archivesuser/moma/drmc/'
 locations_dict = {'preIngest':['pre-ingest_staging','',['']], 'runComponent':['pre-ingest_staging/_run_component_script','',['']], 'readyForIngest':['ready_for_ingest','',['']], 'artworkBacklog':['Artwork_level_backlog','',['']]}
 
 for location in locations_dict:
