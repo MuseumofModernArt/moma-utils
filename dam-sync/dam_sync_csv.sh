42da4ef0bfeb36338da3fe71c1671cf01a620f6c
root <root@vmdrmc02.museum.moma.org>
2015-06-18 15:52:30 -0400

    asdf

--- dam-sync/dam_sync_csv.sh
+++ dam-sync/dam_sync_csv.sh
@@ -3,12 +3,12 @@
 clear
 
 echo "syncing DAM CSV..."
-
+echo "here is the CSV contents"
+cat /home/archivesuser/drmc_dam_bulk/CSV/metadata.csv
+echo "making backup of CSV"
 cp /home/archivesuser/drmc_dam_bulk/CSV/metadata.csv /home/archivesuser/drmc_dam_bulk/csvbackups/$(date -d "today" +"%Y%m%d%H%M").metadata.csv
+echo "moving metadata.csv to DAM sync dir"
 mv /home/archivesuser/drmc_dam_bulk/CSV/metadata.csv /home/archivesuser/DRMC_DAM/CSV
-
 echo "listing CSV watch folder after mv"
-
+ls /home/archivesuser/DRMC_DAM/CSV
 echo "Done!"
-
-mail -s "DRMC metadata available" -t jennifer_sellar@moma.org, david_garfinkel@moma.org, ben_fino-radin@moma.org <<< "New DRMC metadata has just been synced..."
--- tmux.sh
+++ tmux.sh
@@ -5,6 +5,6 @@ tmux rename-window 'Foo'
 tmux select-window -t foo:0
 tmux split-window -h 'tail -f /var/log/archivematica/automate-transfer.log'
 tmux split-window -v -t 0 ''
-tmux split-window -v -t 1 'watch -n 1 ls ~/drmc_dam_bulk/Assets/'
+tmux split-window -v -t 1 'watch -n 1 "ls -lh ~/drmc_dam_bulk/Assets/ | wc -l && du -h ~/drmc_dam_bulk/Assets/"'
 tmux split-window -v -t 3 'watch -n 1 tail ~/drmc_dam_bulk/CSV/metadata.csv'
 tmux -2 attach-session -t foo
