f824f1d455c5292aba144fcea39b438acaa379a2
Ben Fino-Radin <bfinoradin@gmail.com>
2015-08-21 14:58:01 -0400

    added running tally

--- pre-ingest-metrics/size.py
+++ pre-ingest-metrics/size.py
@@ -12,7 +12,7 @@ def get_size(start_path = '.'):
             try:
                 fp = os.path.join(dirpath, f)
                 total_size += os.path.getsize(fp)
-                print fp
+                print total_size+" bytes counted"+" ::::: "+fp
             except OSError, e:
                 print e
     return total_size
