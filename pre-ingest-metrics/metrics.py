b453d94984e1c4be6e0024dee7b61fc46e7c1556
Ben Fino-Radin <bfinoradin@gmail.com>
2015-08-21 14:35:45 -0400

    fp

--- pre-ingest-metrics/metrics.py
+++ pre-ingest-metrics/metrics.py
@@ -17,7 +17,7 @@ def get_size(start_path = '.'):
             try:
                 fp = os.path.join(dirpath, f)
                 total_size += os.path.getsize(fp)
-                print f
+                print fp
             except OSError, e:
                 print e
     return total_size
