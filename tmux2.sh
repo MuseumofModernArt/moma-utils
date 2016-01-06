30f70c3002321f91eede9308dfba44f99e3e46bc
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-06 16:49:39 -0500

    fixed

--- tmux2.sh
+++ tmux2.sh
@@ -4,12 +4,14 @@ tmux new-session -d -s foo 'htop'
 tmux rename-window 'Foo'
 tmux select-window -t foo:0
 tmux split-window -v 'tail -f /var/log/archivematica/automate-transfer.log'
-tmux resize-pane -U 7
-tmux split-window -h 'tail -f /var/log/archivematica/automate-transfer.log' #this will be atom worker log on vrmdrmcatom
-tmux resize-pane -U 7
-tmux resize-pane -R 23
+#tmux resize-pane -U 7
+tmux split-window -h 'top'
+tmux resize-pane -U 10
+tmux resize-pane -R 25
 tmux select-pane -t 1
-tmux split-window -v 'tail -f /var/log/archivematica/automate-transfer.log' #this will be second automation-tools log
-tmux select-pane -t 2
-tmux split-window -h 'df -h' #disk monitoring
+tmux split-window -v 'man top'
+tmux resize-pane -U 10
+tmux split-window -v 'watch -n100 df -h' #disk monitoring
+tmux resize-pane -U 5
 tmux -2 attach-session -t foo
+
