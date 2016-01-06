22865223d6111a2a42d6518ca1cf69897253625d
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-06 16:14:39 -0500

    debugging

--- tmux2.sh
+++ tmux2.sh
@@ -11,5 +11,5 @@ tmux resize-pane -R 23
 tmux select-pane -t 1
 tmux split-window -v 'tail -f /var/log/archivematica/automate-transfer.log' #this will be second automation-tools log
 tmux select-pane -t 2 
-tmux split-window -v 'df -h'
+tmux split-window -h 'df -h' #disk monitoring
 tmux -2 attach-session -t foo
