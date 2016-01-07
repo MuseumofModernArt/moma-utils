d43165d1a00031a0f1f76e86c936764961b63721
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-07 14:59:12 -0500

    quotes

--- tmux2.sh
+++ tmux2.sh
@@ -6,7 +6,7 @@ tmux new-session -d -s foo 'htop'
 tmux rename-window 'Foo'
 tmux select-window -t foo:0
 tmux set-window-option remain-on-exit on
-tmux split-window -v 'watch -n1 "echo 'test' && tail -f /var/log/archivematica/automate-transfer.log"'
+tmux split-window -v 'watch -n1 echo "test"'
 tmux resize-pane -U 28
 tmux split-window -v 'tail -f /var/log/archivematica/automate-transfer.log'
 tmux resize-pane -U 28
@@ -14,16 +14,16 @@ tmux split-window -v 'watch -n100 df -h'
 tmux resize-pane -U 22
 tmux split-window -v "watch -n100000 df -h"
 tmux resize-pane -U 18
-tmux split-window -v 'watch -n100 ls /home/archivesuser'
+tmux split-window -v 'echo 'nothing goes here''
 tmux select-pane -t 3
-tmux split-window -h 'echo 'nothing goes here''
+tmux split-window -h ''
 tmux resize-pane -L 25
 tmux select-pane -t 6
 tmux resize-pane -U 6
 tmux split-window -h 'watch -n100 ls /home/archivesuser/dam02'
 tmux resize-pane -R 26
-tmux set -g pane-border-style fg=black
-tmux set -g pane-active-border-style fg=black
+# tmux set -g pane-border-style fg=black
+# tmux set -g pane-active-border-style fg=black
 tmux -2 attach-session -t foo
 
 
