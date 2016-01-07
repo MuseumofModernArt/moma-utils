7c29e45437c8112531a91cbebbfa71cf569a4115
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-07 13:56:41 -0500

    oops

--- tmux2.sh
+++ tmux2.sh
@@ -12,7 +12,7 @@ tmux resize-pane -U 28
 tmux split-window -v 'watch -n100 df -h'
 tmux resize-pane -U 22
 tmux split-window -v 'watch -n100 ls /var/log/archivematica/'
-tmux resize-pane -U 10
+tmux resize-pane -U 18
 tmux split-window -v 'watch -n100 ls /home/archivesuser'
 tmux select-pane -t 3
 tmux split-window -h 'watch -n100 ls /home/archivesuser'
@@ -21,8 +21,6 @@ tmux select-pane -t 6
 tmux resize-pane -U 6
 tmux split-window -h 'watch -n100 ls /home/archivesuser/dam02'
 tmux resize-pane -R 26
-tmux select-pane -t 5
-tmux resize-pane -U 15
 tmux set -g pane-border-style fg=black
 tmux set -g pane-active-border-style fg=black
 tmux -2 attach-session -t foo
