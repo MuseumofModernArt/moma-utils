438b3f4c3399dd867e23f9a6969598c6b0fdcf9b
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-07 13:53:36 -0500

    color

--- tmux2.sh
+++ tmux2.sh
@@ -18,11 +18,13 @@ tmux select-pane -t 3
 tmux split-window -h 'watch -n100 ls /home/archivesuser'
 tmux resize-pane -L 25
 tmux select-pane -t 6
-tmux resize-pane -U 4
+tmux resize-pane -U 6
 tmux split-window -h 'watch -n100 ls /home/archivesuser/dam02'
 tmux resize-pane -R 26
-# tmux set -g pane-border-style fg=black
-# tmux set -g pane-active-border-style fg=black
+tmux select-pane -t 6
+tmux resize-pane -U 6
+tmux set -g pane-border-style fg=black
+tmux set -g pane-active-border-style fg=black
 tmux -2 attach-session -t foo
 
 
