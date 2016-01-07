732a80ddf1d9d4e07942a9543a5262ba86c9fdd4
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-07 13:19:43 -0500

    resize

--- tmux2.sh
+++ tmux2.sh
@@ -6,6 +6,8 @@ tmux new-session -d -s foo 'htop'
 tmux rename-window 'Foo'
 tmux select-window -t foo:0
 tmux split-window -v 'tail -f /var/log/archivematica/automate-transfer.log'
+tmux select-pane -t 1
+tmux resize-pane -U 23
 tmux split-window -v 'man top'
 tmux split-window -v 'man hop'
 
