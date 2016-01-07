dca26a7ca5620a6f71ce513f02505e50b07620f4
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-07 13:39:15 -0500

    first h split

--- tmux2.sh
+++ tmux2.sh
@@ -13,6 +13,10 @@ tmux split-window -v 'watch -n100 df -h'
 tmux resize-pane -U 22
 tmux split-window -v 'watch -n100 ls /var/log/archivematica/'
 tmux resize-pane -U 10
+tmux split-window -v 'watch -n100 ls /home/archivesuser'
+tmux select-pane -t 3
+tmux split-window -h 'watch -n100 ls /home/archivesuser'
+tmux resize-pane -R 25
 # tmux set -g pane-border-style fg=black
 # tmux set -g pane-active-border-style fg=black
 tmux -2 attach-session -t foo
