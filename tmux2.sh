36b3e5521624c5246be56b23b47b0ec3b2ce8ff9
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-07 13:17:10 -0500

    initial portrait layout

--- tmux2.sh
+++ tmux2.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
+
+#### portrait
 tmux new-session -d -s foo 'htop'
 tmux rename-window 'Foo'
 tmux select-window -t foo:0
-tmux split-window -v 'watch -n1 bash part2.sh'
-#tmux resize-pane -U 7
-tmux split-window -h 'top'
-tmux resize-pane -U 10
-tmux resize-pane -R 25
-tmux select-pane -t 1
+tmux split-window -v 'tail -f /var/log/archivematica/automate-transfer.log'
 tmux split-window -v 'man top'
-tmux resize-pane -U 10
-tmux split-window -v 'watch -n100 df -h' #disk monitoring
-tmux resize-pane -U 5
-tmux -2 attach-session -t foo
+tmux split-window -v 'man hop'
 
+
+
+
+
+#### landscape
+
+# tmux new-session -d -s foo 'htop'
+# tmux rename-window 'Foo'
+# tmux select-window -t foo:0
+# tmux split-window -v 'watch -n1 bash part2.sh'
+# tmux split-window -h 'top'
+# tmux resize-pane -U 10
+# tmux resize-pane -R 25
+# tmux select-pane -t 1
+# tmux split-window -v 'man top'
+# tmux resize-pane -U 10
+# tmux split-window -v 'watch -n100 df -h' #disk monitoring
+# tmux resize-pane -U 5
+# tmux -2 attach-session -t foo
+# tmux set -g pane-border-style fg=black
+# tmux set -g pane-active-border-style fg=black
