52ddee7c4ed0d75da42a29d4a59e61aa7a36fe5d
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-08 17:08:15 -0500

    initial checks

--- makedirs.py
+++ makedirs.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
-import argparse, os, shutil
+import argparse, os, shutil, subprocess, bagit
 
-parser = argparse.ArgumentParser(description="script for making a bunch of random directories")
+parser = argparse.ArgumentParser(description="script for making a bunch of directories with a bunch of files in them")
 parser.add_argument('-t', '--target', type=str, required=True, help='where to put the test data')
 parser.add_argument('-r', '--reels', type=int, required=True, help='how many "reels" to make')
 parser.add_argument('-f', '--files', type=int, required=True, help='how many files per reel')
 args = parser.parse_args()
+
+def walklevel(some_dir, level=1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
 
 target = args.target
 
@@ -23,11 +33,16 @@ while (x <= args.reels):
 		shutil.rmtree(target+'/'+dirname)
 		# print "deleting "+target+'/'+dirname
 	os.mkdir(target+'/'+dirname)
+	os.mkdir(target+'/'+dirname+'/'+dirname)
 	# print "just made "+target+'/'+dirname
 	while (dpx_num <= args.files):
-		filename = target+'/'+dirname+'/'+dirname+str(dpx_num)+".dpx"
+		filename = target+'/'+dirname+'/'+dirname+'/'+dirname+str(dpx_num)+".dpx"
 		open(filename,'w')
 		# print "writing"+filename
 		dpx_num += 1
 	dpx_num = 0001
 	x += 1
+
+for subdir, dirs, files in walklevel(target, 0):
+	for dir in dirs:
+		bag = bagit.make_bag(target+'/'+dir, {'Contact-Name': 'Ben Fino-Radin'})
--- warhol.py
+++ warhol.py
@@ -1,5 +1,60 @@
 #!/usr/bin/env python
 
+import argparse, os, re
+
+parser = argparse.ArgumentParser(description="script for doing automated QA on the Warhol scans")
+parser.add_argument('-s', '--source', type=str, required=True, help='the root that contains the bags')
+# parser.add_argument('-d', '--destination', type=str, required=True, help='where to send bags that pass QA')
+args = parser.parse_args()
+
+
+re1='((?:[a-z][a-z0-9_]*))'	# Variable Name 1
+re2='---'	# Non-greedy match on filler
+re3='((?:[a-z][a-z0-9_]*))'	# Variable Name 2
+re4='---'	# Non-greedy match on filler
+re5='((?:[a-z][a-z]+))'	# Word 1
+
+pattern = re.compile(re1+re2+re3+re4+re5,re.IGNORECASE|re.DOTALL)
+
+
+number_of_reels = 0
+
+def walklevel(some_dir, level=1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
+
+for subdir, dirs, files in walklevel(args.source, 0):
+	for dir in dirs:
+		number_of_reels += 1
+	print "there are "+str(number_of_reels)+" reels to check"
+	print "checking name convention conformence for reel folders"
+	matches = 0
+	mismatches = 0
+	for dir in dirs:
+		# print "check"
+		if pattern.match(dir):
+			matches += 1 
+			# print "match!"
+		else:
+			mismatches += 1
+			# print "fail"
+	print str(matches)+" reels meet the naming convention"
+	print str(mismatches)+" reels failed the naming convention"
+
+
+
+
+
+
+
+
+
 # for each folder in target
 
 	# does it adhere to the naming convention?
