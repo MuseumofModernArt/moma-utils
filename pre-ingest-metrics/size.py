a58c4cf7d32b20c01e2af3c48c19674d970e4107
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-15 16:01:24 -0500

    update printing

--- pre-ingest-metrics/size.py
+++ pre-ingest-metrics/size.py
@@ -19,8 +19,7 @@ def get_size(start_path = '.'):
                 total_size += os.path.getsize(fp)
                 print str(total_size)+" bytes / "+str(size(total_size))+" counted"+" <------------ current position: "+start_path+" : "+f
                 for location in locations_dict:
-                    print location+": "+locations_dict[location][1]
-                stdout.flush()
+                    print location+": "+locations_dict[location][1]"                                                       \r"
             except OSError, e:
                 print e
     return total_size
