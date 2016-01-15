9816d97c36be2df48be52648b3b425a512338ce0
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-15 15:08:47 -0500

    typo

--- pre-ingest-metrics/size.py
+++ pre-ingest-metrics/size.py
@@ -13,7 +13,7 @@ def get_size(start_path = '.'):
             try:
                 fp = os.path.join(dirpath, f)
                 total_size += os.path.getsize(fp)
-                print str(size(total_size))+" bytes counted"+" ::::: current position: "+start_path+" // "+f
+                print str(size(total_size))+" counted"+" ::::: current position: "+start_path+" // "+f
             except OSError, e:
                 print e
     return total_size
