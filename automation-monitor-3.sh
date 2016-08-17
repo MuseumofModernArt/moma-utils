0211560a2b720c414292c44f28ebad999084cfe0
Ben Fino-Radin <bfinoradin@gmail.com>
2016-08-17 13:52:30 -0400

    resize

--- automation-monitor-3.sh
+++ automation-monitor-3.sh
@@ -9,7 +9,8 @@ tmux split-window -v "watch --color --no-title 'bash tail1.sh'"
 
 tmux resize-pane -U 20
 
-#tmux split-window -v -t 1 'watch -n 1 "ls -lh ~/drmc_dam_bulk/Assets/ | wc -l && du -h ~/drmc_dam_bulk/Assets/"'
+tmux split-window -v -t 1 'watch -n 1 "ls -lh ~/drmc_dam_bulk/Assets/ | wc -l && du -h ~/drmc_dam_bulk/Assets/"'
+tmux resize-pane -D 20
 #tmux split-window -v -t 3 'watch -n 1 tail ~/drmc_dam_bulk/CSV/metadata.csv'
 tmux -2 attach-session -t vert
 
