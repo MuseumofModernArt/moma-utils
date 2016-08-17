8e62878d58f92a4190c654159e5acc0b86d0c163
Ben Fino-Radin <bfinoradin@gmail.com>
2016-08-17 13:40:08 -0400

    typo

--- tail1.sh
+++ tail1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-echo -e '\033[32mVNX unbaged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 20 /var/log/archivematica/automate-transfer.log echo -e '\033[32mVNX bagged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 20 /var/log/archivematica/automate-transfer.log
+echo -e '\033[32mVNX unbaged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 20 /var/log/archivematica/automate-transfer.log && echo -e '\033[32mVNX bagged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 20 /var/log/archivematica/automate-transfer.log
