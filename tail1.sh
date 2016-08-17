b372484bd19904eebcd9e7db52c7d7fd6104fd23
Ben Fino-Radin <bfinoradin@gmail.com>
2016-08-17 13:46:41 -0400

    actual paths

--- tail1.sh
+++ tail1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-echo -e '\033[32mVNX unbaged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 5 /var/log/archivematica/automate-transfer.log && echo -e '\033[32mVNX bagged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 5 /var/log/archivematica/automate-transfer.log
+echo -e '\033[32mPipeline 1: VNX unbaged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 5 /var/log/archivematica/automate-transfer.log && echo -e '\033[32mPipeline 2: VNX bagged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 5 /usr/lib/archivematica/automation-tools-2/transfers/automate-transfer.log && echo -e '\033[32mPipeline 3: Standard Isilon workflow >>>>>>>>>>>>>>>>>>>>\033[0m' && tail -n 5 /usr/lib/archivematica/automation-tools-3/transfers/automate-transfer.log
