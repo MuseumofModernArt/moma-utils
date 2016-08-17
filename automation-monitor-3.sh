be9fd1beb84e4eace5a5f23492ebc2a86619247f
Ben Fino-Radin <bfinoradin@gmail.com>
2016-08-17 13:43:03 -0400

    resize pane

--- automation-monitor-3.sh
+++ automation-monitor-3.sh
@@ -7,9 +7,7 @@ tmux rename-window 'vert'
 tmux select-window -t vert:0
 tmux split-window -v "watch --color 'bash tail1.sh'"
 
-tmux split-window -v "watch --color -n 1 "echo '\033[32mVNX unbaged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail /var/log/archivematica/automate-transfer.log""
-
-tmux split-window -v "watch --color -n 1 "echo '\033[32mVNX unbaged >>>>>>>>>>>>>>>>>>>>\033[0m' && tail /var/log/archivematica/automate-transfer.log""
+resize-pane -U 20
 
 tmux split-window -v -t 1 'watch -n 1 "ls -lh ~/drmc_dam_bulk/Assets/ | wc -l && du -h ~/drmc_dam_bulk/Assets/"'
 tmux split-window -v -t 3 'watch -n 1 tail ~/drmc_dam_bulk/CSV/metadata.csv'
